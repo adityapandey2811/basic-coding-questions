78004b5d5659f311779512ae7b78d1d51813591a
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-09-06 19:33:12 +0530

    Create PointersBasic7.c

---
+++ PointersBasic7.c
@@ -0,0 +1,17 @@
+//Pointers and array
+#include<stdio.h>
+void main()
+{
+
+	int arr[] = {0, 1, 2, 3, 4};
+	int *p[] = {arr, arr+1, arr+2, arr+3, arr+4}; 
+	int **ptr = p; 
+	ptr++;
+	printf("%d %d %d\n", ptr-p, *ptr-arr, **ptr);
+	*ptr++; //ptr++;
+	printf("%d %d %d\n", ptr-p, *ptr-arr, **ptr); 
+	*++ptr; 
+	printf("%d %d %d\n", ptr-p, *ptr-arr, **ptr); 
+	++*ptr; 
+	printf("%d %d %d\n", ptr-p, *ptr-arr, **ptr);
+}

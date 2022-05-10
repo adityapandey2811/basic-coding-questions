f30daa72b014368dd968ecb30a6e25d1da67ce5f
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2022-05-10 08:47:10 +0530

    Update BestTimeToBuyAndSellStock.cpp

--- BestTimeToBuyAndSellStock.cpp
+++ BestTimeToBuyAndSellStock.cpp
@@ -1,3 +1,4 @@
+//1st on leetcode (I)
 int maxProfit(vector<int>& prices) {
         int i, buy = prices[i], maxDiff = 0;
         for(i=1;i<prices.size();i++){
@@ -7,4 +8,17 @@ int maxProfit(vector<int>& prices) {
                 maxDiff = max(maxDiff,prices[i] - buy);
         }
         return maxDiff;
+    }
+//@nd on leetcode (II)
+int maxProfit(vector<int>& prices) {
+        int i, buy = prices[i], totalDiff = 0;
+        for(i=1;i<prices.size();i++){
+            if(buy>prices[i])
+                buy = prices[i];
+            else if(prices[i] - buy > 0){
+                totalDiff += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return totalDiff;
     }

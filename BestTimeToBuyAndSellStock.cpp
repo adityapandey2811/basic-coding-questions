64de481783af89ab5049ca8d67aeba7cc6de7ce4
Aditya <2811adityapandey@gmail.com>
2022-07-29 16:04:24 +0530

    Adding new files to branch

--- BestTimeToBuyAndSellStock.cpp
+++ BestTimeToBuyAndSellStock.cpp
@@ -1,24 +1,29 @@
-//1st on leetcode (I)
-int maxProfit(vector<int>& prices) {
-        int i, buy = prices[i], maxDiff = 0;
-        for(i=1;i<prices.size();i++){
-            if(buy>prices[i])
-                buy = prices[i];
-            else
-                maxDiff = max(maxDiff,prices[i] - buy);
-        }
-        return maxDiff;
+// 1st on leetcode (I)
+int maxProfit(vector<int> &prices)
+{
+    int i, buy = prices[i], maxDiff = 0;
+    for (i = 1; i < prices.size(); i++)
+    {
+        if (buy > prices[i])
+            buy = prices[i];
+        else
+            maxDiff = max(maxDiff, prices[i] - buy);
     }
-//@nd on leetcode (II)
-int maxProfit(vector<int>& prices) {
-        int i, buy = prices[i], totalDiff = 0;
-        for(i=1;i<prices.size();i++){
-            if(buy>prices[i])
-                buy = prices[i];
-            else if(prices[i] - buy > 0){
-                totalDiff += prices[i] - buy;
-                buy = prices[i];
-            }
+    return maxDiff;
+}
+// 2nd on leetcode (II)
+int maxProfit(vector<int> &prices)
+{
+    int i, buy = prices[i], totalDiff = 0;
+    for (i = 1; i < prices.size(); i++)
+    {
+        if (buy > prices[i])
+            buy = prices[i];
+        else if (prices[i] - buy > 0)
+        {
+            totalDiff += prices[i] - buy;
+            buy = prices[i];
         }
-        return totalDiff;
     }
+    return totalDiff;
+}

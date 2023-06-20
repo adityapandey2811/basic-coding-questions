15ebe707d41faacd8209547d96008e187634e51d
Aditya Pandey <2811adityapandey@gmail.com>
2023-06-20 17:37:27 +0530

    Update FrogJump.cpp

--- FrogJump.cpp
+++ FrogJump.cpp
@@ -1,3 +1,19 @@
+//Space Optimized DP Solution using Tabulation
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    int prev = 0, prev2 = 0, curi = 0;
+    for(int i=1;i<n;i++){
+        int fs = prev + abs(heights[i] - heights[i-1]);
+        int ss = INT_MAX;
+        if(i > 1) ss = prev2 + abs(heights[i] - heights[i-2]);
+        curi  = min(fs,ss);
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
+
 // DP Solution using Tabulation
 #include <bits/stdc++.h> 
 int frogJump(int n, vector<int> &heights)
